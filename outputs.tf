b044f601356924977b8e7d730741667045fb3072
CloudDrove CI <84795582+clouddrove-ci@users.noreply.github.com>
2025-11-25 23:39:34 +0100

    fix: add output for this.name

--- outputs.tf
+++ outputs.tf
@@ -33,3 +33,9 @@ output "custom_dns_zone_names" {
     if try(cfg.zone_name, null) != null
   }
 }
+
+output "this_name" {
+  description = "The resource name"
+  value       = azurerm_private_dns_zone.this.name
+}
+
